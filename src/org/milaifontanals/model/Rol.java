bbc2ba030964971daafd08d24450023c0fbc020c
mespejo@milaifontanals.org <marionaespejo@gmail.com>
2022-06-03 14:10:34 +0200

    Actualitzacions pujada

--- src/org/milaifontanals/model/Rol.java
+++ src/org/milaifontanals/model/Rol.java
@@ -7,6 +7,19 @@ public class Rol implements Serializable{
     protected Rol(){
         
     }
+
+    public Rol(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
     
     private int id;
     private String nom;
